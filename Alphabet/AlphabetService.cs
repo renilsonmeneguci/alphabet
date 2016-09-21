e0e3717cfaa6c6df9610bfeac4c744868af71379
Renilson Meneguci <renilson.meneguci@crivelli.com.br>
2016-09-21 11:01:01 -0300

    Add project files.

---
+++ Alphabet/AlphabetService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alphabet
+{
+    public static class AlphabetService
+    {
+        private static Dictionary<int, String> alphabet = new Dictionary<int, String>()
+        {
+            {1, "a" },
+            {2, "b" },
+            {3, "c" },
+            {4, "d" },
+            {5, "e" },
+            {6, "f" },
+            {7, "g" },
+            {8, "h" },
+            {9, "i" },
+            {10, "j" },
+            {11, "k" },
+            {12, "l" },
+            {13, "m" },
+            {14, "n" },
+            {15, "o" },
+            {16, "p" },
+            {17, "q" },
+            {18, "r" },
+            {19, "s" },
+            {20, "t" },
+            {21, "u" },
+            {22, "v" },
+            {23, "w" },
+            {24, "x" },
+            {25, "y" },
+            {26, "z" }
+        };
+
+        public static String GetCharacterFromNumber(int number, bool lower = true)
+        {
+            if (number <= 0 || number >= 25) return String.Empty;
+
+            var character = alphabet.First(a => a.Key == number).Value;
+
+            if (!lower)
+            {
+                return character.ToUpper();
+            }
+
+            return character;
+        }
+
+        public static int GetNumberFromCharacter(string character)
+        {
+            if (String.IsNullOrEmpty(character)) return 0;
+
+            var number = alphabet.FirstOrDefault(a => a.Value == character.ToLower());
+
+            return number.Key;
+        }
+    }
+}
---
+++ AlphabetSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using Alphabet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var character = AlphabetService.GetCharacterFromNumber(10);
+            Console.WriteLine("Character:" + character);
+
+            var number = AlphabetService.GetNumberFromCharacter("j");
+            Console.WriteLine("Number:" + number);
+
+            Console.ReadKey();
+        }
+    }
+}
